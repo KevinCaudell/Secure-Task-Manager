7a553d37ad51195520f8b6ebb62edee46ff52071
Kevin Caudell <kevin.caudell.1@gmail.com>
2026-02-16 21:46:42 -0500

    Add initial implementation of task manager with styles, views, and routing

--- server.js
+++ server.js
@@ -11,12 +11,32 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.use(layouts);
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
+// Home Page
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+// Login Page
+app.get("/login", (req, res) => {
+    res.render("login", {layout: false});
+});
+
+// Sign up page
+app.get("/signup", (req, res) => {
+    res.render("signup", {layout: false});
+});
+
+// Dashboard page
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard")
+});
+
+// Tasks page
+app.get("/tasks", (req, res) => {
+    res.render("tasks")
 });
 
 app.listen(PORT, ()=>{
