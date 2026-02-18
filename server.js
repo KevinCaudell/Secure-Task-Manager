e93bd259eb678ce5b082067bae7b1f645eedb2f4
Kevin Caudell <kevin.caudell.1@gmail.com>
2026-02-18 00:06:25 -0500

    Implement user signup functionality with validation and password hashing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const layouts = require("express-ejs-layouts");
 const db = require("./database/db.js");
+const { hashPassword, verifyPassword } = require("./utilities/hashing.js");
 
 //Initiliaze app
 const app = express();
@@ -26,9 +27,73 @@ app.get("/login", (req, res) => {
     res.render("login", {layout: false});
 });
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    const user = db.prepare(`
+        SELECT * FROM USER WHERE EMAIL = ?
+        `).get(email);
+
+    if (!user) {
+        return res.send('User not found!');
+    };
+
+    if (verifyPassword(password, user.SALT, user.PASSWORD_HASH)){
+        db.prepare(`
+            UPDATE USER SET LAST_LOGIN = CURRENT_TIMESTAMP WHERE ID = ?
+            `).run(user.ID);
+        res.redirect('/dashboard');
+    }else{
+        res.send("Incorrect password");
+    };
+});
+
 // Sign up page
 app.get("/signup", (req, res) => {
-    res.render("signup", {layout: false});
+    const { email, username, password } = req.body;
+
+    const forbiddenChars = /[<>\/\\'"`]/;
+    const requiredSpecialChars = /[!#$%&*]/;
+
+    if (email.length > 60) {
+        return res.send("Email is too long!");
+    };
+
+    if (username.length > 25){
+        return res.send('Username is too long!');
+    };
+
+    if (password.length < 8){
+        return res.send('Password must be atleast 8 characters!');
+    };
+
+    if (forbiddenChars.test(password)){
+        return res.send('Password contains forbidden characters: < > / \\ \' " `');
+    };
+
+    if (!requiredSpecialChars.test(password)){
+        return res.send('Password must include at least one special character: ! # $ % & *');
+    };
+
+
+    const { salt, hashed } = hashPassword(password);
+
+    try {
+        const insertToDB = db.prepare(`
+            INSERT INTO USER (EMAIL, USERNAME, PASSWORD_HASH, SALT)
+            VALUES(?, ?, ?, ?)    
+        `);
+        insertToDB.run(email, username, hashed, salt);
+
+        res.redirect("/login");
+    } catch (err) {
+        if (err.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+            res.send("That email is already in use!");
+        }else{
+            console.error(err);
+            res.send('An unexpected error occurred!');
+        };
+    };
 });
 
 // Dashboard page
